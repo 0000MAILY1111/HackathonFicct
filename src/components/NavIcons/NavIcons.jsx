2a5cb1fd150846a7b31b87552fd15cf1151ce3a6
0000MAILY1111 <valdezmelizza19@gmail.com>
2024-11-21 13:36:16 -0400

    ordericons

--- src/components/NavIcons/NavIcons.jsx
+++ src/components/NavIcons/NavIcons.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-import swim from "../../assets/Swin.svg";
-import gym from "../../assets/GYM.svg";
-import health from "../../assets/Health.svg";
+
 import tourism from "../../assets/tourism.svg";
 import education from "../../assets/education.svg";
 import bici from "../../assets/Bici.svg";
+import swim from "../../assets/Swin.svg";
+import gym from "../../assets/GYM.svg";
+import health from "../../assets/Health.svg";
 import "./NavIcons.css"  
+
 
 const NavIcons = () => {
   return (
     <div className="icon-container">
-      <img src={swim} alt="icono" className="icon" onClick={() => handleClick ('swim')} />
-      <img src={gym} alt="icono" className="icon" onClick={() => handleClick ('gym')} />
-      <img src={health} alt="icono" className="icon" onClick={() => handleClick ('healt')} />
+    
       <img src={tourism} alt="icono" className="icon" onClick={() => handleClick ('tourism')} />
       <img src={education} alt="icono" className="icon"  onClick={() => handleClick ('education')}/>
       <img src={bici} alt="icono" className="icon"  onClick={() => handleClick ('bici')}/>
-
+      <img src={swim} alt="icono" className="icon" onClick={() => handleClick ('swim')} />
+      <img src={gym} alt="icono" className="icon" onClick={() => handleClick ('gym')} />
+      <img src={health} alt="icono" className="icon" onClick={() => handleClick ('healt')} />
         
     </div>
   );
